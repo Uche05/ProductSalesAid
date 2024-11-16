42e63b60546a7fdeb5083bda4c72fb9d3b04b355
Uchechukwu Kpadeuwa <uchekpadeuwa@gmail.com>
2024-11-16 12:44:03 +0000

    Get all needed data; companyname, country, emal, and 3 numbers and update to spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-#'Pip3 freeze > requirements.txt'. to put in the dependenices into the requirements.txt file
-# do not forget to validate data
 import re
 
 import gspread
@@ -8,14 +6,14 @@ from google.oauth2.service_account import Credentials
 # Define the scope, which is a list of google services we are accessing
 SCOPE = [ "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive.file",
-        "https://www.googleapis.com/auth/drive" ]
+        "https://www.googleapis.com/auth/drive"]
 
-#Authenticate the code to access the Company Google-spreadsheet
-CREDS = Credentials.from_service_account_file('creds.json') 
+# Authenticate the code to access the Company Google-spreadsheet
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
-#Open the spreadsheet
+# Open the spreadsheet
 SHEET = GSPREAD_CLIENT.open('Product-Sales-Aid')
 
 def welcome():
@@ -24,34 +22,34 @@ def welcome():
     """
     print("Welcome to the Product Sales Aid!")
     print("This program will help you analyze product sales data......")
-    
+
 
 def get_company_data():
     """
     Obtains the company basic data of company_name and country
-    
+
     Returns:
         list: [company_name,country_of_company]
     """
     while True:
-        print("Enter the name of your company and country, separated by commas\n")
+        print("Enter the name of your company, country, separated by commas\n")
         print("Like this: companyName,companyCountry ")
         company = input("Enter the name of the company, and country: ")
         company = company.split(",")
-        
+
         if validate(company):
             print("Data is valid")
             break
-        
+
     return company
 
 def validate(values):
     """
     Validates the input data to ensure it contains exactly two values.
-    
+
     Args:
         values (list): List containing company name and country.
-    
+
     Returns:
         bool: True if valid, False otherwise.
     """
@@ -66,34 +64,20 @@ def validate(values):
         return False
     return True
 
-def update_company_data(company, worksheet):
-    """
-    Receives a list of company details and
-    Updates the company data in spreadsheet in needed worksheet.
-    
-    Args:
-        company (list): A list containing two values.
-    """
-    print("Updating the spreadsheet.....")
-    sheet_to_update = SHEET.worksheet(worksheet)
-    sheet_to_update.append_row(company)
-    print(f"{worksheet} is updating.....")
-    print("Done!")
-
 def get_email():
     """
     Obtains the email of the company from the company_name
-    
+
     Args:
-        company (str): Name of the company.
-    
+        input -> company (str): Name of the company.
+
     Returns:
         str: Email of the company.
     """
     company_email = input("Enter your company's email: ")
     email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     emails = re.findall(email_regex, company_email)
-        
+
     if emails:
         return emails[0]
     else:
@@ -103,41 +87,58 @@ def get_email():
 def get_numeric_data():
     """
     Obtains the number of products requested by the company
-    
+
     Args:
-        
-    
+        input -> company (int): Name of the company.
+
     Returns:
-        
+        list: Number of products required currently only 3
     """
     product = []
-    for i in range(1,4):
+    for i in range(1, 4):
         print(f"Please enter the number of product{i} you want")
         product_no = input(f"Please enter the number of product{i} you want: ")
-        
+
         try:
             product.append(int(product_no))
             if not int(product_no):
                 raise ValueError
         except ValueError as e:
-            print(f"Error: {e}, please ensure you insert a number! \n Try Again!!!")
-        
-    return {product}
-    
+            print(f"Error: {e}, please ensure you insert a number!\n")
+
+    return product
+
+
+def update_company_data(company, worksheet):
+    """
+    Receives a list of company details and
+    Updates the company data in spreadsheet in needed worksheet.
+
+    Args:
+        company (list): A list containing 6 values.
+    """
+    print("Updating the spreadsheet.....")
+    sheet_to_update = SHEET.worksheet(worksheet)
+    sheet_to_update.append_row(company)
+    print(f"{worksheet} is updating.....")
+    print("Done!")
+
 
 def main():
     """
     Executes all program functions.
     """
+    welcome()
     company_data = get_company_data()
-    
-    if validate(company_data):
-        update_company_data(company_data, "INFO1")
-    else:
-        print("Try again!!!")
-    
-    
-#if __name__ == "__main__":
-#    main()
+    email = get_email()
+    product_data = get_numeric_data()
+    print("Got all necessary data....")
 
-get_numeric_data()
+    additional_data = [email] + product_data
+    company_data.extend(additional_data)
+    update_company_data(company_data, "INFO1")
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
